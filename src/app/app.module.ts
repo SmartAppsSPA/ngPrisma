27d8ab384adf3a8a2c908cacf17e66eb294eb0c5
jamoragar <javier.moragarojas@gmail.com>
2017-09-14 18:27:36 -0300

    Update - Actualizacion archivo json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { CargaComponent } from './components/carga/carga.component';
 import { FotosComponent } from './components/fotos/fotos.component';
 import { CodeComponent } from './components/code/code.component';
-import { QrComponent } from './components/qr/qr.component';
 
 //Rutas
 import { app_routing } from './app.routes';
@@ -17,12 +16,9 @@ import { FIREBASE_CONFIG } from './config/firebase.config';
 import { CargaImagenesService } from './services/carga-imagenes.service';
 //directives
 import { NgDropFilesDirective } from './directives/ng-drop-files.directive';
-
 //Para inputs
 import { FormsModule } from '@angular/forms';
 
-//QR
-import { NgxQRCodeModule } from 'ngx-qrcode2';
 
 
 @NgModule({
@@ -31,7 +27,6 @@ import { NgxQRCodeModule } from 'ngx-qrcode2';
     CargaComponent,
     FotosComponent,
     CodeComponent,
-    QrComponent,
     NgDropFilesDirective
   ],
   imports: [
@@ -39,8 +34,7 @@ import { NgxQRCodeModule } from 'ngx-qrcode2';
     app_routing,
     FormsModule,
     AngularFireModule.initializeApp(FIREBASE_CONFIG),
-    AngularFireDatabaseModule,
-    NgxQRCodeModule
+    AngularFireDatabaseModule
   ],
   providers: [
     CargaImagenesService
